8bc33db41b21b8a461d23e8f3b4e1a8e3cd2ad79
Vinicius Perrone <perronevinicius2018@gmail.com>
2022-10-14 18:32:57 -0300

    chore: add typeorm, dataSource and credentials

--- src/shared/infra/http/server/server.ts
+++ src/shared/infra/http/server/server.ts
@@ -1,13 +1,18 @@
 import express from 'express';
 import cors from 'cors';
 import router from '../routes';
+import dotenv from 'dotenv';
+import { dataSource } from '@shared/infra/typeorm';
 
-const app = express();
 
-app.use(cors());
-app.use(express.json());
-app.use(router)
-
-app.listen(3333, () => {
-  console.log('Server started on port 3333');
-})
+dataSource.initialize().then(() => {
+  const app = express();
+  
+  app.use(cors());
+  app.use(express.json());
+  app.use(router)
+  
+  app.listen(3333, () => {
+    console.log('Server started on port 3333');
+  })
+});
