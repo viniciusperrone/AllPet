6cd301ed0a752a9d8c2e1e3efd38826928ec3985
Vinicius Perrone <perronevinicius2018@gmail.com>
2022-10-15 02:49:04 -0300

    feat: add container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,1 +1,9 @@
 import { container } from 'tsyringe';
+
+import { IUsersRepository } from '@modules/users/domain/repositories/IUsersRepository';
+import { UsersRepository } from '@modules/users/infra/typeorm/repositories/UsersRepository';
+
+container.registerSingleton<IUsersRepository>(
+  'UsersRepository',
+  UsersRepository,
+);
--- src/shared/infra/http/server/server.ts
+++ src/shared/infra/http/server/server.ts
@@ -1,18 +1,19 @@
+import 'reflect-metadata';
+import '@shared/container';
 import express from 'express';
 import cors from 'cors';
 import router from '../routes';
 import dotenv from 'dotenv';
 import { dataSource } from '@shared/infra/typeorm';
 
-
 dataSource.initialize().then(() => {
   const app = express();
-  
+
   app.use(cors());
   app.use(express.json());
-  app.use(router)
-  
+  app.use(router);
+
   app.listen(3333, () => {
     console.log('Server started on port 3333');
-  })
+  });
 });
