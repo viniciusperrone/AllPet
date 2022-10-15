a143825387c586b002416d1203d3863fa9304bca
Vinicius Perrone <perronevinicius2018@gmail.com>
2022-10-15 03:00:19 -0300

    feat: added custom handle error

--- src/shared/infra/http/server/server.ts
+++ src/shared/infra/http/server/server.ts
@@ -1,10 +1,11 @@
 import 'reflect-metadata';
 import '@shared/container';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import router from '../routes';
 import dotenv from 'dotenv';
 import { dataSource } from '@shared/infra/typeorm';
+import AppError from '@shared/errors/AppError';
 
 dataSource.initialize().then(() => {
   const app = express();
@@ -12,6 +13,27 @@ dataSource.initialize().then(() => {
   app.use(cors());
   app.use(express.json());
   app.use(router);
+
+  app.use(
+    (
+      error: Error,
+      request: Request,
+      response: Response,
+      next: NextFunction,
+    ) => {
+      if (error instanceof AppError) {
+        return response.status(error.statusCode).json({
+          status: 'error',
+          message: error.message,
+        });
+      }
+
+      return response.status(500).json({
+        status: 'error',
+        message: 'Internal server error',
+      });
+    },
+  );
 
   app.listen(3333, () => {
     console.log('Server started on port 3333');
