be2c2adf3f8cd8f359df6390407e87c970ae2bc8
Vinicius Perrone <perronevinicius2018@gmail.com>
2022-10-16 15:03:01 -0300

    fix: error

--- src/shared/infra/http/server/server.ts
+++ src/shared/infra/http/server/server.ts
@@ -10,37 +10,40 @@ import dotenv from 'dotenv';
 import { dataSource } from '../../typeorm';
 import AppError from '../../../errors/AppError';
 
-dataSource.initialize().then(() => {
-  const app = express();
+dataSource
+  .initialize()
+  .then(() => {
+    const app = express();
 
-  app.use(cors());
-  app.use(express.json());
-  app.use(router);
-  app.use('/files', express.static(uploadConfig.directory));
+    app.use(cors());
+    app.use(express.json());
+    app.use(router);
+    app.use('/files', express.static(uploadConfig.directory));
 
-  app.use(errors());
-  app.use(
-    (
-      error: Error,
-      request: Request,
-      response: Response,
-      next: NextFunction,
-    ) => {
-      if (error instanceof AppError) {
-        return response.status(error.statusCode).json({
+    app.use(errors());
+    app.use(
+      (
+        error: Error,
+        request: Request,
+        response: Response,
+        next: NextFunction,
+      ) => {
+        if (error instanceof AppError) {
+          return response.status(error.statusCode).json({
+            status: 'error',
+            message: error.message,
+          });
+        }
+
+        return response.status(500).json({
           status: 'error',
-          message: error.message,
+          message: 'Internal server error',
         });
-      }
+      },
+    );
 
-      return response.status(500).json({
-        status: 'error',
-        message: 'Internal server error',
-      });
-    },
-  );
-
-  app.listen(3333, () => {
-    console.log('Server started on port 3333');
-  });
-});
+    app.listen(3333, () => {
+      console.log('Server started on port 3333');
+    });
+  })
+  .catch(error => console.log);
